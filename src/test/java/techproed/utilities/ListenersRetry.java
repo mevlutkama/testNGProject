96a844ba674a8ef529c4d01f998bcd1f90569673
Mevlut Kama <mtkama02@gmail.com>
2023-01-21 19:45:33 +0300

    testNG

--- src/test/java/techproed/utilities/ListenersRetry.java
+++ src/test/java/techproed/utilities/ListenersRetry.java
@@ -15,4 +15,11 @@ public class ListenersRetry implements IRetryAnalyzer{
         }
 }
 
+/*
+        JUnit: A testing framework. Has less annotation compare to TestNG
+TestNG: An advanced testing framework. Advanced version of JUnit framework
+Page Object Model: Popular Framework Design. We use page  object model for creating reusable page objects and methods.
+Maven: Project built tool. With maven, we can easily add other dependencies(junit,testng,selenium, faker, poi,…) to our project. Instead of importing the jar files, we just add the dependency in the pom.xml file. Then maven automatically imports jar pies for us. Developers may also use maven for their their project built.
+Selenium: Selenim is used to test web based application. We can also do mobile testing with 3rd party tool such as appium.
+*/
 
